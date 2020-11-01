184bbd4a0515bc554a7f013adbca349ddfc707ee
d4wnin9 <d4wnin9@gmail.com>
2020-11-01 11:11:24 +0900

    Add comment

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use std::io::prelude::*;
 
 
 pub fn run(args: clap::ArgMatches) -> Result<(), Box<dyn Error>> {
+    // fileへのパス
     let mut path_to_gdbs = dirs::home_dir().unwrap();
     path_to_gdbs.push(".gdbs");
     let mut path_to_gdbinit = dirs::home_dir().unwrap();
@@ -16,10 +17,12 @@ pub fn run(args: clap::ArgMatches) -> Result<(), Box<dyn Error>> {
 
     let gdb_type = argparse(args);
 
+    // .gdbsの読み込み
     let mut f = File::open(&path_to_gdbs)?;
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
+    // source gdb
     let source_path = if gdb_type == "/bin/gdb" {
         gdb_type
     } else {
@@ -33,6 +36,7 @@ pub fn run(args: clap::ArgMatches) -> Result<(), Box<dyn Error>> {
 }
 
 fn argparse(args: clap::ArgMatches) -> String {
+    // 条件にあったらそのgdbの種類を返す
     let mut gdb_type = "/bin/gdb".to_string();
     if let Some(arg) = args.value_of("type") {
         gdb_type = arg.to_string();
@@ -53,6 +57,7 @@ fn argparse(args: clap::ArgMatches) -> String {
 fn search(gdb_type: &str, contents: &str) -> String {
     let mut source_path = String::new();
 
+    // gdbsからgdbの種類の一致するのを返す
     for line in contents.lines() {
         if line.contains(gdb_type) {
             source_path = line.to_string()
